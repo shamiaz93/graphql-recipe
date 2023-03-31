195de01aff2b1550f0e0b832df7af5a3559b8913
shamiaz93 <63896432+shamiaz93@users.noreply.github.com>
2023-03-31 21:21:07 +0530

    Added documentation added to schema

--- app.js
+++ app.js
@@ -6,23 +6,72 @@ var { buildSchema } = require('graphql');
 
 // GraphQL schema
 var schema = buildSchema(`
+    """
+    Queries to return object or array of object of recipe details by recipe id or user id
+    """
     type Query {
+        """
+        Returns an object of recipe details by recipe id
+        """
         recipe(id: Int!): Recipe
+        """
+        Returns list of recipe as array of objects by user id
+        """
         recipes(userId: Int): [Recipe]
     },
+    """
+    Mutations to create recipe, update recipe and save or share recipe
+    """
     type Mutation {
+        """
+        Mutation to update recipe object using recipe id , name ,description , ingredients and method as input
+        """
         updateRecipe(id: Int!, name: String, description: String, ingredients: [String], method: [String], url: String): Recipe
+        """
+        Mutation to create recipe object using recipe name author name ,description and ingredients and method as input
+        """        
         createRecipe(name: String!, author: String, description: String, ingredients: [String], method: [String], url: String, userId: Int!): Recipe
+        """
+        Mutation to save or share recipe object using recipe id , User id  as input
+        """        
         saveOrShareRecipe(recipeId: Int!, userId: Int!): [Recipe]
     },
+
+    """
+    Recipe object with properties, id, name, url, description, author, ingredients, method, url
+    """
     type Recipe {
+        """
+        Id of the recipe object, it is a unique field
+        """
         id: Int
+        """
+        String value to hold name of the recipe 
+        """
         name: String
+        """
+        String value to represent recipe url 
+        """
         url: String
+        """
+        String value to provide description about the recipe 
+        """
         description: String
+        """
+        String value to hold the name of the author 
+        """
         author: String
+        """
+        Array of string to provide details about ingredients 
+        """
         ingredients: [String]
+        """
+        Method to return string array of recipes 
+        """
         method: [String],
+        """
+        Int value to hold user id 
+        """
         userId: [Int]
     }
 `);
